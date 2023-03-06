21286c2d0b63597e519b14db84cd46e3224941e4
Marione Fiszman <marionef1985@gmail.com>
2023-03-06 17:50:08 +0000

    erase extra file

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,7 +8,6 @@ import Footer from "./Footer.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-import Manicure from "./Marione.jsx";
 
 //create your first component
 const Home = () => {
